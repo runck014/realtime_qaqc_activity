4b0ed09d1d68b8c5cbc043e3f1617fdd8983d8d7
Bryan Runck <runck014@umn.edu>
2023-02-23 12:20:51 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import sqlalchemy
+import numpy as np
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    matrix = np.random.rand(3,2)
+    return matrix
+
+
+@app.route("/hello")
+def hello():
+    return "hello"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
