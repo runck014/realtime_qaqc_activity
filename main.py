11dbe9bf47524627ded39ddcf1d9db0a53cfafd8
Bryan Runck <runck014@umn.edu>
2023-02-23 15:17:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import sqlalchemy
 import numpy as np
 from flask import Flask
+from datetime import datetime
+import json
 
 app = Flask(__name__)
 
@@ -15,6 +17,43 @@ def hello_world():
 @app.route("/hello")
 def hello():
     return "hello"
+
+
+@app.route("/5572/milk_truck_sensor")
+def milk_truck_sensor():
+    '''
+    Returns lat, lon, current time, and temperature sensor.
+    All are randomly nulled 5% of the API calls.
+    '''
+    
+    lat = np.random.normal(46.7296, 6, 1)[0]
+    lon = np.random.normal(-94, 10, 1)[0]
+    
+    now = datetime.now()
+    current_time = now.strftime("%D:%H:%M:%S")
+    
+    air_temperature = np.random.normal(31, 15, 1)[0]
+    
+    if np.random.uniform(0.0,1.0,1)[0] < 0.05:
+        air_temperature = 'nan'
+
+    if np.random.uniform(0.0,1.0,1)[0] < 0.05:
+        lat = 'nan'
+        
+    if np.random.uniform(0.0,1.0,1)[0] < 0.05:
+        lon = 'nan'
+    
+    if np.random.uniform(0.0,1.0,1)[0] < 0.05:
+        current_time = '01/21/29:15:11:12'
+    
+    response_dict = {
+        'lat':lat,
+        'lon':lon,
+        'current_time':current_time,
+        'air_temperature':air_temperature}
+    
+    return json.dumps(response_dict)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
